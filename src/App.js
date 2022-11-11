eb5e8cb6037292f6c34b727cf7d83678d9315ab7
BhaweshPandey <pandeybhawesh12@gmail.com>
2022-11-11 17:08:08 +0530

    Components-library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './Components/Cardcomponent/Card';
+import TextInput from './Components/TextInput/TextInput';
 function App() {
   return (
     <div className="App">
       <Card />
+      <TextInput label={"name :"} type={"text"} placeholder={"Name"} />
+      <TextInput label={"Password :"} type={"Password"} placeholder={"Password"} />
     </div>
   );
 }
